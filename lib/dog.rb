7a0df6c8f6e15ff86f92f18ac11cb4093f33953e
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-31 15:24:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+# Add your code 
+
+class Dog 
+  attr_accessor :name
+  @@all = [  ]
+  
+ def initialize(name)
+   @name = name
+   @@all << name
+ end
+ 
+ def all
+   return @@all
+  end
+end
